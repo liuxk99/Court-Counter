4a2659fe00f770c3f5ba1f97a410d0ab04d6beed
Thomas Liu <liuxk99@gmail.com>
2022-05-27 16:50:29 +0800

    <Build>:Configure AGP and port to AndroidX.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -16,11 +16,10 @@
 package com.example.android.courtcounter;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * This activity keeps track of the basketball score for 2 teams.
